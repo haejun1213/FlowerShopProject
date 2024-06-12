994ca96f84db84577e8de65fbb413ffccb896516
haejun1213 <162427472+haejun1213@users.noreply.github.com>
2024-06-12 21:26:50 +0900

    Update CartItem.java

--- src/flowershop/model/CartItem.java
+++ src/flowershop/model/CartItem.java
@@ -1,12 +1,13 @@
 package flowershop.model;
 
+import java.util.Arrays;
+
 public class CartItem {
 
 	Flower flower;
 	int flowerID;
 	int quantity;
 
-	
 	public int getFlowerID() {
 		return flowerID;
 	}
@@ -20,29 +21,33 @@ public class CartItem {
 		this.flowerID = flower.getFlowerID();
 		this.quantity = 1;
 	}
-	
+
 	public Flower getFlower() {
 		return flower;
 	}
+
 	public void setflower(Flower flower) {
 		this.flower = flower;
 	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public void addQuantity(int quantity) {
 		this.quantity += quantity;
 	}
-	
+
 	public int getPrice() {
 		return flower.getPrice() * quantity;
 	}
-	
-	public String toString() {
-		return flower.getFlowerID() + ", " + flower.getType() + ", " + flower.getName() + ", " + flower.getColor() + ", " + quantity + "개, "+ getPrice() + "원";
-}
-}
 
+	public String toString() {
+		return flower.getFlowerID() + ", " + flower.getType() + ", " + flower.getName() + ", " + flower.getColor()
+				+ ", " + flower.getComposition() + ", " + quantity + "개, " + getPrice() + "원";
+	}
+}
